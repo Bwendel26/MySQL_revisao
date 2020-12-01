21cdc9cc59a5e9728b2e08174d7e42f27cbe429c
brunoFernandes <bwendel26999@gmail.com>
2020-12-01 14:15:38 -0300

    Iniciando repo de revisao da linguagem SQL e MySQL database...

---
+++ consultarEstados.sql
@@ -0,0 +1,15 @@
+select * from estados
+
+SELECT 
+    sigla, 
+    nome as 'Nome do Estado' 
+from estados
+where regiao = 'sul';
+
+select 
+    nome, 
+    regiao, 
+    populacao 
+from `estados`
+where populacao >= 10
+order by populacao;
